bab69fd97752cb70881a78072ac87c6831674b80
“Rec-choi” <1820758693@qq.com>
2023-01-03 02:58:44 +0800

    修改

--- src/router/modules/ecology.example.ts
+++ src/router/modules/ecology.example.ts
@@ -2,7 +2,7 @@
  * @Author: “Rec-choi” “1820758693@qq.com”
  * @Date: 2023-01-01 00:33:16
  * @LastEditors: “Rec-choi” “1820758693@qq.com”
- * @LastEditTime: 2023-01-03 02:02:52
+ * @LastEditTime: 2023-01-03 02:44:54
  * @FilePath: \KAUNGJIA\src\router\modules\ecology.example.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ const routes: RouteRecordRaw[] = [
     path: '/official',
     redirect: '/official/fantastic-startkit',
     meta: {
-      title: '官方周边',
+      title: 'CODE存储',
       icon: 'ep:grid',
     },
     children: [
@@ -24,8 +24,8 @@ const routes: RouteRecordRaw[] = [
         redirect: '',
         name: 'officialFantasticStartkit',
         meta: {
-          title: 'Fantastic-startkit 项目启动套件',
-          link: 'https://hooray.gitee.io/fantastic-startkit/',
+          title: 'Gitee',
+          link: 'https://gitee.com/rechoi/bolg.git',
         },
       },
       {
